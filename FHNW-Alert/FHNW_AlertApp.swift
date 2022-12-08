3bf1171fee1cbda53b7e924ce64b58b1ddb7c60e
Boehme, Ingo <ingo.boehme@zuehlke.com>
2022-12-08 10:38:38 +0100

    Alert sample

---
+++ FHNW-Alert/FHNW_AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  FHNW_AlertApp.swift
+//  FHNW-Alert
+//
+//  Created by Ingo Boehme on 08.12.22.
+//
+
+import SwiftUI
+
+@main
+struct FHNW_AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
