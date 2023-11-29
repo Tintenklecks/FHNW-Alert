81c2b74e831fcb74ca817d84762801f9de1c7a35
Ingo Boehme <ingo.boehme@zuehlke.com>
2023-11-29 22:55:29 +0100

    With ActionSheet

--- FHNW-Alert/FHNW-Alert/ContentView.swift
+++ FHNW-Alert/FHNW-Alert/ContentView.swift
@@ -10,63 +10,22 @@ import SwiftUI
 // MARK: - ContentView
 
 struct ContentView: View {
-    @State private var showAlert = false
-    @State private var showAlert2 = false
-
-    @State private var text = ""
-
-    @State private var showError = false
-    @State private var errorText = ""
+    @State private var showingSheet = false
 
     var body: some View {
-        VStack {
-            // MARK: - Button 1 -
-
-            Button("Show alert") {
-                showAlert.toggle()
-            }.alert(isPresented: $showAlert) {
-                Alert(title: Text("I am the title"),
-                      message: Text("I am the message"),
-                      dismissButton: .destructive(Text("Destructive")))
-            }
-
-            // MARK: - Button 2 -
-
-            Button("Show alert with two buttons") {
-                showAlert2.toggle()
-            }.alert(isPresented: $showAlert2) {
-                Alert(title: Text("Title for 2 buttons"),
-                      primaryButton: .default(Text("Default")),
-                      secondaryButton: .destructive(Text("Kaputt")) {
-                          print("Make it KAPUTTTTTTT")
-                      })
-            }
-
-            // MARK: - Button 3 -
-
-            Divider()
-                .padding()
-
-            TextField("Enter some error text", text: $text, axis: .vertical)
-                .padding()
-                .background(Color.white)
-                .cornerRadius(10)
-                .overlay(RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color.black, lineWidth: 0.5))
-                .shadow(radius: 5)
-
-            Button("Simulate change of error text") {
-                errorText = text
-            }.alert(isPresented: $showError) {
-                Alert(title: Text("Error: "),
-                      message: Text(errorText))
-            }
+        Button("Show Action Sheet") {
+            showingSheet = true
         }
-        .buttonStyle(BorderedProminentButtonStyle())
-
-        .padding()
-        .onChange(of: errorText) { newValue in
-            showError = newValue != ""
+        .actionSheet(isPresented: $showingSheet) {
+            ActionSheet(
+                title: Text("Options"),
+                message: Text("Choose an option"),
+                buttons: [
+                    .default(Text("Option 1")) { /* Action for Option 1 */ },
+                    .default(Text("Option 2")) { /* Action for Option 2 */ },
+                    .cancel() // This adds a cancel button
+                ]
+            )
         }
     }
 }
